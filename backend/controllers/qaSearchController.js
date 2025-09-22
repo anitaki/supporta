69ed00a75faa1d08f629933aff6f673579d4391c
Anita Angelidou <anitawebdesigns@gmail.com>
2025-09-22 08:38:40 +0300

    completed search embeddings, and return response to query from openai

--- backend/controllers/qaSearchController.js
+++ backend/controllers/qaSearchController.js
@@ -2,6 +2,7 @@ const QA = require("../models/qaModel");
 const saveQAEmbedding = require("../utils/saveQAEmbedding");
 const { createEmbedding } = require("../utils/embeddings");
 const mongoose = require("mongoose");
+const OpenAI = require("openai");
 
 const searchQAs = async (req, res) => {
   const { q } = req.query;
@@ -12,31 +13,59 @@ const searchQAs = async (req, res) => {
 
   // Vector search user query in MongoDB
   const results = await QA.aggregate([
-  {
-    $vectorSearch: {
-      index: "qa_embedding_index",   
-      path: "embedding",            
-      queryVector: queryEmbedding,  
-      numCandidates: 100,         
-      limit: 5,
-      filter: {
-        businessId: new mongoose.Types.ObjectId(req.user.businessId)
-      }
-    }
-  },
-  
-  {
-    $project: {
-      question: 1,
-      answer: 1,
-      score: { $meta: "vectorSearchScore" }
-    }
-  }
-]);
+    {
+      $vectorSearch: {
+        index: "qa_embedding_index",
+        path: "embedding",
+        queryVector: queryEmbedding,
+        numCandidates: 100,
+        limit: 5,
+        filter: {
+          businessId: new mongoose.Types.ObjectId(req.user.businessId),
+        },
+      },
+    },
 
+    {
+      $project: {
+        question: 1,
+        answer: 1,
+        score: { $meta: "vectorSearchScore" },
+      },
+    },
+  ]);
 
+  // Get response from openAI
+  const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
 
-  res.json(results);
+  const context = results
+    .map((res) => `Q: ${res.question}\nA:${res.answer}`)
+    .join("\n");
+
+  const messages = [
+    {
+      role: "system",
+      content:
+        "You are a helpful support assistant. Answer only using the provided context. If the answer is not in the context, say you don't know.",
+    },
+    {
+      role: "user",
+      content: `Context: 
+      ${context.trim()}
+      User question: 
+      ${q.trim()}`,
+    },
+  ];
+
+  const response = await openai.responses.create({
+    model: "gpt-4o-mini-2024-07-18",
+    input: messages,
+    temperature: 1.1
+  });
+
+  res.json(response.output_text);
 };
 
 module.exports = searchQAs;
